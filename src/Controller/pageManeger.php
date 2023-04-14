ce845f1db85e2abe29be6194ae986c0c64291e2c
Kartik Patel <kartik@simformsolutions.com>
2023-04-14 19:34:10 +0530

    add

--- src/Controller/pageManeger.php
+++ src/Controller/pageManeger.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Employee;
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Name;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Routing\Annotation\Route;
 
 class pageManeger extends AbstractController
@@ -14,15 +20,43 @@ class pageManeger extends AbstractController
         return $this->render("mainManu/home.html.twig");
     }
 
-    #[Route("/", name: "app_insert")]
-    public function appInsert()
+    #[Route("/new", name: "app_insert")]
+    public function appInsert(EntityManagerInterface $em)
     {
-        return $this->render("mainManu/home.html.twig");
+        $emp = new Employee();
+        $emp->setName("kartik");
+        $emp->setAddress("luna");
+        $emp->setNumber(77575);
+        $emp->setCompony("Simform");
+        $em->persist($emp);
+        $em->flush();
+        return $this->render(
+            "mainManu/insert.html.twig",
+            ["id" => $emp->getId()]
+        );
     }
 
-    #[Route("/", name: "app_show")]
-    public function appShow()
+    #[Route("/show", name: "app_show")]
+    public function appShow(EmployeeRepository $emp)
     {
-        return $this->render("mainManu/home.html.twig");
+        $emp = $emp->getAllOrderById();
+        // dd($emp);
+
+        return $this->render("mainManu/show.html.twig", ["emp" => $emp]);
+    }
+    #[Route("/delete/{id}", name: "user_delete")]
+    public function userDelete(int $id, EmployeeRepository $emprepo, EntityManagerInterface $em)
+    {
+        $ob = $emprepo->find($id);
+        if ($ob) {
+            $em->remove($ob);
+            $em->flush();
+            $this->addFlash('success', "Record Deleted");
+            dd($em);
+        } else {
+            dd("Not valid id");
+        }
+
+        // return $this->redirectToRoute("app_show");
     }
 }
---
+++ src/Entity/Employee.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+class Employee
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $number = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $compony = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getCompony(): ?string
+    {
+        return $this->compony;
+    }
+
+    public function setCompony(string $compony): self
+    {
+        $this->compony = $compony;
+
+        return $this;
+    }
+}
